c79da589b0e785ddc76294556480c2612ad2ea4d
Fabio Dorneles <fabio.dorneles@stone.com.br>
2024-06-19 20:10:40 -0300

    adicionando area abaixo do nome

--- app/src/main/java/com/fabiodrneles/dashboardmodel/MainActivity.kt
+++ app/src/main/java/com/fabiodrneles/dashboardmodel/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,13 +15,17 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -74,9 +80,7 @@ fun Dashboard() {
                 modifier = Modifier
                     .padding(top = 48.dp, start = 24.dp, end = 24.dp)
                     .fillMaxWidth()
-                    .constrainAs(profile) {
-                        top.linkTo(topImg.bottom)
-                    }
+
             ) {
                 Column(
                     modifier = Modifier
@@ -87,11 +91,53 @@ fun Dashboard() {
                     horizontalAlignment = Alignment.Start
                 ) {
                     Text(
-                        text = "hello",
+                        text = "Hello",
                         color = Color.White,
                         fontSize = 18.sp
                     )
+
+                    Text(text = "Fabio Dorneles",
+                        color = Color.White,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 14.dp)
+                    )
                 }
+
+                Image(painter = painterResource(id = R.drawable.profile),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(100.dp)
+                        .clickable {}
+                )
+            }
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp, start = 24.dp, end = 24.dp)
+                    .shadow(3.dp, shape = RoundedCornerShape(20.dp))
+                    .background(color = Color.White, shape = RoundedCornerShape(20.dp))
+                    .constrainAs(profile) {
+                        top.linkTo(topImg.bottom)
+                        bottom.linkTo(topImg.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            ){
+                Column (
+                    modifier = Modifier
+                        .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
+                        .height(90.dp)
+                        .weight(90.dp)
+                        .background(color = Color(android.graphics.Color.parseColor("#ffe0c8")))
+                ){
+
+                }
+
+
             }
         }
     }
