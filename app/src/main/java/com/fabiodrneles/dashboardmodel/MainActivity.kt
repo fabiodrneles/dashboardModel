85cdf242cf61c3440aa7cbc954b1df11a2596aac
Fabio Dorneles <fabio.dorneles@stone.com.br>
2024-06-18 23:17:18 -0300

    construindo tela login

--- app/src/main/java/com/fabiodrneles/dashboardmodel/MainActivity.kt
+++ app/src/main/java/com/fabiodrneles/dashboardmodel/MainActivity.kt
@@ -4,44 +4,95 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.fabiodrneles.dashboardmodel.ui.theme.DashboardModelTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DashboardModelTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Dashboard()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Dashboard() {
+    // Crie o Brush para o gradiente
+    val gradientBrush = Brush.horizontalGradient(
+        colors = listOf(
+            Color(android.graphics.Color.parseColor("#EA6D35")),
+            Color(android.graphics.Color.parseColor("#3b608c"))
+        )
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DashboardModelTheme {
-        Greeting("Android")
+    Column(
+        Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(color = Color(android.graphics.Color.parseColor("#f8eeec"))),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ConstraintLayout {
+            val (topImg, profile) = createRefs()
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(245.dp)
+                    .constrainAs(topImg) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                    // Aplique o Brush ao Modifier.background
+                    .background(
+                        brush = gradientBrush,
+                        shape = RoundedCornerShape(bottomEnd = 40.dp, bottomStart = 40.dp)
+                    )
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 48.dp, start = 24.dp, end = 24.dp)
+                    .fillMaxWidth()
+                    .constrainAs(profile) {
+                        top.linkTo(topImg.bottom)
+                    }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .padding(start = 14.dp)
+                        .weight(0.7f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = "hello",
+                        color = Color.White,
+                        fontSize = 18.sp
+                    )
+                }
+            }
+        }
     }
 }
